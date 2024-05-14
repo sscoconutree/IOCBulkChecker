473a45402672133d40fe7b99d375f21a610943ae
d3m0nt1m3 <59388557+sscoconutree@users.noreply.github.com>
2024-05-14 18:50:21 +0800

    Update helpers.js

--- helpers.js
+++ helpers.js
@@ -13,12 +13,12 @@ function isIPv6Address(input) {
 }
 
 function isURL(input) {
-    const URLregex = /^((https?:\/\/)|(www\.))[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()!@:%_\+.~#?&\/\/=]*)$/;
+    const URLregex = /^(https?:\/\/)(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()!@:%_\+.~#?&\/\/=]*)$/;
     return URLregex.test(input);
 }
 
 function isDomain(input) {
-    const domainRegex = /^(?!www\.)(?!https?:\/\/)([a-zA-Z0-9-]{1,63}\.)+[a-zA-Z]{2,6}(\/.*)?$/;
+    const domainRegex = /^(www\.)?([a-zA-Z0-9-]{1,63}\.)+[a-zA-Z]{2,6}(\/.*)?$/;
     return domainRegex.test(input);
 }
 
